d36f1d67f9944e748b466d9982322052c0a52422
Rx5v <teguhwsaputra11@gmail.com>
2025-07-14 16:02:58 +0700

    migrate to mongodb

--- api/webhook.js
+++ api/webhook.js
@@ -143,8 +143,7 @@ bot.onText(/\/start/, async (msg) => {
     console.log(`[BOT] Received /start command from chat ID: ${chatId}`);
     try {
         console.log('[BOT] Attempting to ensure user...'); // <-- NEW LOG
-        const user = await ensureUser(msg.from);
-        console.log(user);
+        await ensureUser(msg.from);
         
         console.log(`[BOT] User ${msg.from.id} ensured in DB.`); // <-- NEW LOG
          const options = {
